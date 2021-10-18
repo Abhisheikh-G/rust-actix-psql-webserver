9b3d19db5f59fc82bb0daf711d07d27339ac081f
Abhisheikh Gill <abhisheikhg@gmail.com>
2021-10-18 19:27:53 -0400

    Add check_todo and create_todo handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,5 @@
 use crate::db;
-use crate::models::Status;
+use crate::models::{CreateTodoList, ResultResponse, Status};
 use actix_web::{web, HttpResponse, Responder};
 use deadpool_postgres::{Client, Pool};
 
@@ -18,6 +18,51 @@ pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
     let result = db::get_todos(&client).await;
     match result {
         Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn get_items(db_pool: web::Data<Pool>, path: web::Path<(i32,)>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database.");
+
+    let result = db::get_items(&client, path.0).await;
+    match result {
+        Ok(items) => HttpResponse::Ok().json(items),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn create_todo(
+    db_pool: web::Data<Pool>,
+    json: web::Json<CreateTodoList>,
+) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database.");
+
+    let result = db::create_todo(&client, json.title.clone()).await;
+    match result {
+        Ok(todo) => HttpResponse::Ok().json(todo),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn check_todo(db_pool: web::Data<Pool>, path: web::Path<(i32, i32)>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database.");
+
+    let result = db::check_todo(&client, path.0, path.1).await;
+    match result {
+        Ok(()) => HttpResponse::Ok().json(ResultResponse { success: true }),
+        Err(ref e) if e.kind() == std::io::ErrorKind::Other => {
+            HttpResponse::Ok().json(ResultResponse { success: true })
+        }
         Err(_) => HttpResponse::InternalServerError().into(),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ mod models;
 
 use crate::handlers::*;
 use actix_web::{web, App, HttpServer};
-use deadpool_postgres::Runtime;
+
 use dotenv::dotenv;
 use tokio_postgres::NoTls;
 
-#[actix_web::main]
+#[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let config = crate::config::Config::from_env().unwrap();
-    let pool = config.pg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+    let pool = config.pg.create_pool(NoTls).unwrap();
 
     println!(
         "Starting server at http://{}:{}/",
@@ -25,6 +25,12 @@ async fn main() -> std::io::Result<()> {
             .data(pool.clone())
             .route("/", web::get().to(status))
             .route("/todos{_:/?}", web::get().to(get_todos))
+            .route("/todos{_:/?}", web::post().to(create_todo))
+            .route("/todos/{list_id}/items{_:/?}", web::get().to(get_items))
+            .route(
+                "/todos/{list_id}/items/{item_id}{_:/?}",
+                web::put().to(check_todo),
+            )
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
